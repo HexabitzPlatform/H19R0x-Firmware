6c8be16978ed7447b217c7faa9a960f712357531
Muhammad Alhaddad <ubuntu.msa@gmail.com>
2024-11-12 14:30:11 +0300

    create folder, source and header files for communication with driver in stspin

---
+++ STM32CubeIDE/Commands_Driver_APIs/Commands_Driver_APIs.c
@@ -0,0 +1,25 @@
+/*
+ * Commands_Driver_APIS.C
+ * Description: Sending commands and receiving message from BLDC motor driver.
+ *  Created on: Oct 30, 2024
+ *      Author: Muhammad Alhaddad @ Hexabitz
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 Hexabitz.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
+
+#include"Commands_Driver_APIs.h"
+
+uint8_t TestDebuging()
+{
+	uint8_t c = 0;
+	c = c + 1;
+	c = 15;
+	return 0;
+}
+
+/************************ (C) COPYRIGHT Hexabitz *****END OF FILE****/
---
+++ STM32CubeIDE/Commands_Driver_APIs/Commands_Driver_APIs.h
@@ -0,0 +1,36 @@
+/*
+ * Commands_Driver_APIS.h
+ * Description: Sending commands and receiving message from BLDC motor driver.
+ *  Created on: Oct 30, 2024
+ *      Author: Muhammad Alhaddad @ Hexabitz
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2024 Hexabitz.
+ * All rights reserved.
+ *
+ ******************************************************************************
+ */
+#ifndef COMMANDS_DRIVER_API_H_
+#define COMMANDS_DRIVER_API_H_
+
+
+/* Includes ------------------------------------------------------------------*/
+#include <stdint.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* Exported functions  ---------------------------------------------*/
+
+uint8_t TestDebuging();
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* Commands_Driver_APIS */
+/************************ (C) COPYRIGHT Hexabitz *****END OF FILE****/
